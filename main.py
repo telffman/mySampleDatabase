6172ccb012d97ced709c5b856b7d9429ca5fb353
telffman <101122900+telffman@users.noreply.github.com>
2024-03-19 16:45:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import json
 import psycopg2
 import random
 import base64
+
+secrets = json.load(open('secrets.json'))
+
+host = secrets["HOST"]
+database = secrets["DATABASE"]
+user = secrets["USERNAME"]
+password = secrets["PASSWORD"]
+
+conn = psycopg2.connect(
+    host=host,
+    database=database,
+    user=user,
+    password=password
+)
+
+cursor = conn.cursor()
